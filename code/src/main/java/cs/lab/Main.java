f1b0dc96c5788789d1b7bfc5031244eb71a02551
morphisjustfun <mariojacobo2011@gmail.com>
2021-07-26 16:17:00 -0500

    init

---
+++ code/src/main/java/cs/lab/Main.java
@@ -0,0 +1,12 @@
+package cs.lab;
+
+import java.util.logging.Logger;
+
+public class Main {
+
+    static final Logger logger = Logger.getLogger(Main.class.getName());    
+
+    public static void main(String[] args) {
+        logger.warning("asdasd");
+    }
+}
---
+++ code/src/test/java/cs/lab/MainTest.java
@@ -0,0 +1,18 @@
+package cs.lab;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.logging.Logger;
+
+
+class CSTranslatorTest 
+    static final Logger logger = Logger.getLogger(Main.class.getName());
+
+    @Test
+    public void test1(){
+        assertEquals(3, 3);
+    }
+};
